c414887b2691c4b421defe1cdbd590d7b0806f7f
dev-meine <kamarazainabaziza2002@gmail.com>
2025-05-22 21:47:00 +0000

    Updated AboutPage.tsx

--- src/pages/AboutPage.tsx
+++ src/pages/AboutPage.tsx
@@ -43,7 +43,7 @@ The Federation has sole rights for the organization, development, growth and pro
               viewport={{ once: true }}
             >
               <img 
-                src="https://images.pexels.com/photos/1415810/pexels-photo-1415810.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1" 
+                src="https://i.ibb.co/4RYBzK1j/20240430-112927.jpg" 
                 alt="Swimming competition" 
                 className="w-full h-full object-cover"
               />
