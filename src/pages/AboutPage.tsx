cc6790b16dc695f20355e0e78ccfaa8379580100
dev-meine <kamarazainabaziza2002@gmail.com>
2025-05-22 21:55:25 +0000

    Updated AboutPage.tsx

--- src/pages/AboutPage.tsx
+++ src/pages/AboutPage.tsx
@@ -7,7 +7,7 @@ const AboutPage: React.FC = () => {
     <>
       <PageHeader
         title="About Us"
-        description="Learn about the Sierra Leone Swimming, Diving & Water Polo Association."
+        description="Learn about the Sierra Leone Swimming Aquatics"
         image="https://i.ibb.co/DPc6MhdS/20240430-165712-11zon.jpg"
       />
 
